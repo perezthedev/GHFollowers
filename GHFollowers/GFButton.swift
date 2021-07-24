28594f22c278785c0592d30ea1393303725a440d
Ryan Perez <ryancs115@gmail.com>
2021-07-23 23:44:19 -0700

    added GFTextField class

--- GHFollowers/GFButton.swift
+++ GHFollowers/GFButton.swift
@@ -11,6 +11,7 @@ class GFButton: UIButton {
 
     override init(frame: CGRect){
         super.init(frame: frame)
+        configure()
     }
     
     // this handles initialization for storyboard
@@ -18,5 +19,17 @@ class GFButton: UIButton {
         fatalError("init(coder:) has not been implemented")
     }
     
-
+    init(backgroundColor: UIColor, title: String) {
+        super.init(frame: .zero)
+        self.backgroundColor = backgroundColor
+        self.setTitle(title, for: .normal)
+        configure()
+    }
+    
+    private func configure() {
+        layer.cornerRadius = 10
+        titleLabel?.textColor = .white
+        titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
+        translatesAutoresizingMaskIntoConstraints = false
+    }
 }
