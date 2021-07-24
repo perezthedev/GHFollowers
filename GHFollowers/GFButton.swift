dc3942c47bb0fe723ef6db63776eeeb2fe104770
Ryan Perez <ryancs115@gmail.com>
2021-07-23 22:58:04 -0700

    created GFButton class with initializers

---
+++ GHFollowers/GFButton.swift
@@ -0,0 +1,22 @@
+//
+//  GFButton.swift
+//  GHFollowers
+//
+//  Created by thepercussivedev on 7/23/21.
+//
+
+import UIKit
+
+class GFButton: UIButton {
+
+    override init(frame: CGRect){
+        super.init(frame: frame)
+    }
+    
+    // this handles initialization for storyboard
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+
+}
